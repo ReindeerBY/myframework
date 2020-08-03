869ebf5a48630624be9ad593937c665e0aaa8695
Evgeniy <Shavluga>
2020-08-03 17:01:37 +0300

    Initial commit

---
+++ Example/MyFramework/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyFramework
+//
+//  Created by ReindeerBY on 08/03/2020.
+//  Copyright (c) 2020 ReindeerBY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
